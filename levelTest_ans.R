ea44be6f8496736dc0ef81b474bba71fbde1ce49
elara15598 <elara15598@gmail.com>
2017-03-06 18:43:55 +0800

    ans

---
+++ levelTest_ans.R
@@ -0,0 +1,18 @@
+y<- c(1,0,0,1,1,1,0,1,1)
+which(head(y,-1) == tail(y,-1) & head(y,-1)==1)
+
+raw <- read.delim("data/weather.txt",check.names = F, na.strings = ".")
+library(dplyr)
+raw_n <- melt(raw, id.vars = names(raw)[1:3],variable.name = "day") %>% 
+  dcast(., year + month + day ~ element) %>%
+  mutate(., diff = .[,4]- .[,5]) #%>%
+  #na.omit(.)
+
+library(hflights)
+str(hflights)
+group_by(hflights, UniqueCarrier, Month) %>% 
+  summarise(., quan = quantile(ArrDelay,0.1, na.rm=T)) %>%
+  group_by(., UniqueCarrier) %>%
+  summarise(., mean(quan))
+
+# time: 10min
