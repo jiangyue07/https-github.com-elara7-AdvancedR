d9ecb5467a15358d2678dbac3850fd7ba3ffcb46
elara7 <elara15598@gmail.com>
2017-03-07 23:34:19 +0800

    Update levelTest_ans.R
    add time

--- levelTest_ans.R
+++ levelTest_ans.R
@@ -1,5 +1,6 @@
 y<- c(1,0,0,1,1,1,0,1,1)
 which(head(y,-1) == tail(y,-1) & head(y,-1)==1)
+# time: 2min
 
 raw <- read.delim("data/weather.txt",check.names = F, na.strings = ".")
 library(dplyr)
@@ -8,6 +9,8 @@ raw_n <- melt(raw, id.vars = names(raw)[1:3],variable.name = "day") %>%
   mutate(., diff = .[,4]- .[,5]) #%>%
   #na.omit(.)
 
+# time: 4min
+
 library(hflights)
 str(hflights)
 group_by(hflights, UniqueCarrier, Month) %>% 
@@ -15,4 +18,4 @@ group_by(hflights, UniqueCarrier, Month) %>%
   group_by(., UniqueCarrier) %>%
   summarise(., mean(quan))
 
-# time: 10min
+# time: 4min
